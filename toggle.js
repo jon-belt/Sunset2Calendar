be24e61c3fdf574715bab27f1ce267c87e1d5846
jon-belt <48921235+jon-belt@users.noreply.github.com>
2025-02-08 01:34:28 +0000

    added manifest.json + sunset/ sunrise styling

---
+++ toggle.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.getElementById("changeButton");
+    let toggled = false;
+
+    button.addEventListener("click", function () {
+        console.log("Button was clicked");
+        toggled = !toggled;
+        //change document title
+        document.title = toggled ? "Sunrise2Calendar" : "Sunset2Calendar";
+
+        //change text content
+        document.getElementById("toggleTitle").textContent = toggled ? "Sunrise2Calendar" : "Sunset2Calendar";
+
+        //change image source
+        document.getElementById("toggleImg").src = toggled ? "sunrise.png" : "sunset.png";
+
+        //toggle button
+        button.classList.toggle("toggled");
+        
+        //change border color
+        document.body.style.borderColor = toggled ? "#E0A355" : "#033655";
+    });
+});
